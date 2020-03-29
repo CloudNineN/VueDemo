2cb2d34d8c9b8d5671ac198782ec411b43ab2b20
CloudNineN <1522775963@qq.com>
2020-03-29 22:09:31 +0800

    111

---
+++ src/component/Template.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>this is a Component</h1>
+      <h2>understand</h2>
+  </div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/component/TestMVVM.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <p></p>
+    <label>
+      <input type="text" v-model="msg" />
+    </label>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "hhhhhhhhhh"
+    };
+  }
+};
+</script>
+<style></style>
